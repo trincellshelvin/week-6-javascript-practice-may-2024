b9f239954747d280f261f1336fb7d235e4a78861
Trincell Shelvin <trincellshelvin@gmail.com>
2024-06-29 06:02:39 -0500

    update

--- day-1.js
+++ day-1.js
@@ -11,11 +11,11 @@ function day1Part1() {
   playerOne.hiScore = prompt("What is your all time hi-score?");
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
-
+  playerOne.superpower = prompt("What would you like your super power to be?");
 
   // Try it!
   // TODO: add the answer to the new question here.
-  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore}?`;
+  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore}? Of course you would pick the super power of ${playerOne.superpower}, that is one amazing super power to have, lets see how it will help you beat your previous high score.`;
 
 }
 
@@ -28,12 +28,12 @@ function day1Part2() {
   customer.address = prompt("What is your address?");
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
-  
+  customer.phone = prompt("What is your phone number?");
 
   
   // Try it!
   // TODO: add the answer to the new question to the innerHTML statment below.
-  messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}.`;
+  messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}. With a phone number of ${customer.phone}.`;
 }
 part2.onclick = day1Part2;
 
@@ -44,10 +44,10 @@ function day1Part3() {
 
   // Try it!
   // TODO: create a prompt to ask about a pet
-
+  pet.name = prompt("What is your pet's name?");
   // Try it!
   // TODO: make a message based on the answers to the prompt.
-  messageParagraph.innerHTML = ``;
+  messageParagraph.innerHTML =  `Welcome ${pet.name} to the playground, a helpful place to find advice, meet-ups for play dates, and listings for pet friendly establishments.`;
 }
 part3.onclick = day1Part3;
 
@@ -56,7 +56,10 @@ part3.onclick = day1Part3;
 // TODO: add your own idea for a prompt. Use an object to store the user's response.
 function day1Part4() {
   alert("hello!");
-
+let species;
+species = {};
+species.name = prompt("What species is your pet?");
+messageParagraph.innerHTML = `We are a species friendly environment. We just ask that each patron is respectful of others and their choice of pet.`;
   
 }
 
